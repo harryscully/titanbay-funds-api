7efbd20b466b18540e7284af8a96c8e1c915079c
harryscully <harry.scully@ugogroup.co.uk>
2026-06-06 14:08:39 +0100

    Add 404 and default error handling

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import "dotenv/config"
 import express from "express"
+import type { Request, Response, NextFunction} from "express"
 import { fundsRouter } from "./routes/funds"
 import { investorsRouter } from "./routes/investors"
 
@@ -9,11 +10,22 @@ const app = express()
 
 app.use(express.json())
 
-app.get("/health", (req,res) => {
-    res.json({"status" : "ok"})
+app.get("/health", (req, res) => {
+    res.json({ "status": "ok" })
 })
 
-app.use('/funds',fundsRouter)
-app.use('/investors',investorsRouter)
+app.use('/funds', fundsRouter)
+app.use('/investors', investorsRouter)
+
+app.use((req, res) => {
+    res.status(404).json({ "error": "route not found" })
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (res.headersSent) {
+        return next(err)
+    }
+    res.status(500).json({"error" : "internal server error"})
+})
 
 app.listen(PORT, () => console.log(`server connected on port ${PORT}`))
