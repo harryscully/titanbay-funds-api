4c0da82539bb525203c1d8759c3293b786438efb
harryscully <harry.scully@ugogroup.co.uk>
2026-06-06 12:55:43 +0100

    Add POST /funds route

--- src/routes/funds.ts
+++ src/routes/funds.ts
@@ -6,3 +6,11 @@ export const fundsRouter = express.Router()
 fundsRouter.get('/', async (req,res) => {
     res.json(await prisma.fund.findMany())
 })
+
+fundsRouter.post('/', async (req,res) => {
+    const newFund = await prisma.fund.create({
+        data: {...req.body}
+    })
+    res.status(201)
+        .json(newFund)
+})
