5601392d27797c75970080282d1e618fb558f57b
harryscully <harry.scully@ugogroup.co.uk>
2026-06-06 13:24:46 +0100

    Add PUT /funds route

--- src/routes/funds.ts
+++ src/routes/funds.ts
@@ -11,29 +11,42 @@ function formatFund(fund: Fund) {
     }
 }
 
-fundsRouter.get('/', async (req,res) => {
+fundsRouter.get('/', async (req, res) => {
     const funds = await prisma.fund.findMany()
     const fundsFormatted = funds.map(formatFund)
     res.json(fundsFormatted)
 })
 
-fundsRouter.post('/', async (req,res) => {
+fundsRouter.post('/', async (req, res) => {
     const newFund = await prisma.fund.create({
-        data: {...req.body}
+        data: { ...req.body }
     })
     const newFundFormatted = formatFund(newFund)
     res.status(201)
         .json(newFundFormatted)
 })
 
-fundsRouter.get('/:id', async (req,res) => {
+fundsRouter.get('/:id', async (req, res) => {
     const id = req.params.id
     const fund = await prisma.fund.findUnique({
-        where: {id: id}
+        where: { id: id }
     })
     if (!fund) {
-        res.status(404).json({"error": `no fund found with id: ${id}`})
+        res.status(404).json({ "error": `no fund found with id: ${id}` })
     } else {
         res.json(formatFund(fund))
+    }
+})
+
+fundsRouter.put('/', async (req, res) => {
+    try {
+        const { id, ...data } = req.body
+        const updatedFund = await prisma.fund.update({
+            where: { id: id },
+            data
+        })
+        res.json(formatFund(updatedFund))
+    } catch (err) {
+        res.status(404).json({ "error": "fund not found" })
     }
 })
