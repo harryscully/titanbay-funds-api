dd57e5d8d78c29c398a0c1eb11cf17addecb065f
harryscully <harry.scully@ugogroup.co.uk>
2026-06-06 13:04:31 +0100

    Format target_size_usd as number in GET and POST

--- src/routes/funds.ts
+++ src/routes/funds.ts
@@ -1,16 +1,27 @@
 import express from "express"
 import { prisma } from "../lib/prisma"
+import type { Fund } from "../generated/prisma/client"
 
 export const fundsRouter = express.Router()
 
+function formatFund(fund: Fund) {
+    return {
+        ...fund,
+        target_size_usd: Number(fund.target_size_usd)
+    }
+}
+
 fundsRouter.get('/', async (req,res) => {
-    res.json(await prisma.fund.findMany())
+    const funds = await prisma.fund.findMany()
+    const fundsFormatted = funds.map(formatFund)
+    res.json(fundsFormatted)
 })
 
 fundsRouter.post('/', async (req,res) => {
     const newFund = await prisma.fund.create({
         data: {...req.body}
     })
+    const newFundFormatted = formatFund(newFund)
     res.status(201)
-        .json(newFund)
+        .json(newFundFormatted)
 })
